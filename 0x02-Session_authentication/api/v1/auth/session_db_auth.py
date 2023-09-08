7f0fbe12bcbedd5f84626ae1af93bc7f9f7c3cea
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-09-08 02:59:51 +0100

    authentication

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,59 +1,51 @@
 #!/usr/bin/env python3
 """
-Define class SessionDButh
+define API authentication class session
 """
-from .session_exp_auth import SessionExpAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
+from os import getenv
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
-    """
-    Definition of SessionDBAuth class that persists session data
-    in a database
+    """session DB auth class to manage API authentication
     """
 
     def create_session(self, user_id=None):
-        """
-        Create a Session ID for a user_id
-        Args:
-           user_id (str): user id
-        """
-        session_id = super().create_session(user_id)
-        if not session_id:
-            return None
-        kw = {
-            "user_id": user_id,
-            "session_id": session_id
-        }
-        user = UserSession(**kw)
-        user.save()
-        return session_id
+        """create session"""
+        if user_id:
+            session_id = super().create_session(user_id)
+            us = UserSession(user_id=user_id, session_id=session_id)
+            us.save()
+            UserSession.save_to_file()
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
+        """get user ID from session
         """
-        Returns a user ID based on a session ID
-        Args:
-            session_id (str): session ID
-        Return:
-            user id or None if session_id is None or not a string
-        """
-        user_id = UserSession.search({"session_id": session_id})
-        if user_id:
-            return user_id
-        return None
+        if not session_id:
+            return None
+        UserSession.load_from_file()
+        users = UserSession.search({'session_id': session_id})
+        for u in users:
+            delta = timedelta(seconds=self.session_duration)
+            if u.created_at + delta < datetime.now():
+                return None
+            return u.user_id
 
     def destroy_session(self, request=None):
+        """delete the user session / log out
         """
-        Destroy a UserSession instance based on a
-        Session ID from a request cookie
-        """
-        if request is None:
-            return False
-        session_id = self.session_cookie(request)
-        if not session_id:
-            return False
-        user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            user_session[0].remove()
-            return True
+        if request:
+            session_id = self.session_cookie(request)
+            if not session_id:
+                return False
+            if not self.user_id_for_session_id(session_id):
+                return False
+            users = UserSession.search({'session_id': session_id})
+            for u in users:
+                u.remove()
+                UserSession.save_to_file()
+                return True
         return False
